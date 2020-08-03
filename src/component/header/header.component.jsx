4922f21e953cb515a1dce030ae098fa187b8ffca
Anthony Agbator <tonjoe99@gmail.com>
2020-08-03 23:58:19 +0100

    header test suite

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './header.styles.scss';
+import Logo from '../../assets/logo.svg';
 
 const Header = () => {
     return (
         <header>
-            Header
+            <div className="header-wrapper">
+                <div className="logo">
+                    <img alt="logo" src={Logo} />
+                </div>
+            </div>
         </header>
     )
 }
